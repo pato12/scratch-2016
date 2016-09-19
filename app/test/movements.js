cbb8c65da73609dc1a49285157db552eb893b299
Patricio Perez <pato12p@gmail.com>
2016-09-19 19:40:44 -0300

    Formatear test

--- app/test/movements.js
+++ app/test/movements.js
@@ -1,8 +1,10 @@
 var myApp = {alert: function(){}};
 
-QUnit.test( "El controller tiene que fallar cuando se agrega un movimiento invalido", function( assert ) {
+QUnit.test( "testing al movementController", function( assert ) {
   var controller = new movementController();
   var resultado = controller.agregarMovimiento(-10, new Date().toString(), 'a', 'tipo');
+  var resultado2 = controller.agregarMovimiento(10, new Date().toString(), 'a', 'tipo');
 
-  assert.ok( !resultado, "Paso!" );
+  assert.notOk(resultado, "El controller tiene que fallar cuando se agrega un movimiento invalido." );
+  assert.ok(resultado2, "El controller tiene que funcionar cuando se agrega un movimiento valido." );
 });
