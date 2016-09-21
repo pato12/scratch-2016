77104184c71cebb51fbb1ea2f69684363b2ec9fa
gon <gonza94.4@gmail.com>
2016-09-20 21:35:37 -0300

    ++ columna fecha y se une egreso/ingreso en reportes movimiento

--- app/js/model/itemReport.js
+++ app/js/model/itemReport.js
@@ -1,21 +1,20 @@
 
 //Constructor de UNA fila de la tabla de reportes de gastos
-var itemReport = function (tipo, monto, saldo, id) {
-  this.ingreso = '-';
-  this.egreso = '-';
+var itemReport = function (tipo, monto, saldo, id, fecha) {
+  this.monto = monto;
   this.saldo = saldo;
+  this.fecha = fecha;
+  this.tipo = tipo;
   this.id = id;
 
-  if (tipo === 'ingreso')
-    this.ingreso = monto;
-  else
-    this.egreso = monto;
+  
 };
 // formatear la fila a un Json
 itemReport.prototype.toJSON = function () {
   return {
-    ingreso: (this.ingreso !== '-' ? '$' + this.ingreso : '-'),
-    egreso: (this.egreso !== '-' ? '$' + this.egreso : '-'),
+    monto: (this.tipo === 'egreso' ? '($' +this.monto + ')' : '$' + this.monto),
+    egreso: this.tipo === 'egreso',
+    fecha: this.fecha,
     saldo: (this.saldo < 0 ? '($' + Math.abs(this.saldo) + ')' : '$' + this.saldo),
     saldoNegativo: this.saldo < 0,
     id: this.id
