f2717ecdd37bb7b1db1720443adcbc2ba3b1d5d9
Patricio Perez <pato12p@gmail.com>
2016-09-24 00:47:46 -0300

    oktest

--- app/test/movements.js
+++ app/test/movements.js
@@ -7,4 +7,5 @@ QUnit.test( "testing al movementController", function( assert ) {
 
   assert.notOk(resultado, "El controller tiene que fallar cuando se agrega un movimiento invalido." );
   assert.ok(resultado2, "El controller tiene que funcionar cuando se agrega un movimiento valido." );
+  assert.ok(true, "ok test." );
 });
