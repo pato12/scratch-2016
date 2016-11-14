abd3236576d340bce08d78bd1d7b4d124ad361e0
gonza944 <gonza94.4@gmail.com>
2016-11-14 17:17:21 -0300

    se agregan comentarios

--- app/js/controller/categoriaController.js
+++ app/js/controller/categoriaController.js
@@ -1,7 +1,7 @@
 var categoriaController = function() {
 
 };
-
+//controlador que maneja la lógica del agregado de categorías
 categoriaController.prototype.agregarCategoria = function(nombre, descripcion, esDefault, tipo) {
     if(this.validar(nombre)){
         var newCategoria = new categoria(nombre, descripcion, esDefault, tipo);
@@ -10,7 +10,7 @@ categoriaController.prototype.agregarCategoria = function(nombre, descripcion, e
     }
     return false;
 };
-
+//Validador de campos obligatorios
 categoriaController.prototype.validar = function(nombre) {
     if(nombre.trim() === "") {
         myApp.alert("Debe ingresar un nombre");
